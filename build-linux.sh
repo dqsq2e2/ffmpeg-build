a7f4ee35873d034030118141dc34128ac3c727b6
dqsq2e2 <sjc2523796176@qq.com>
2026-03-10 11:37:19 +0800

    Add files via upload

--- build-linux.sh
+++ build-linux.sh
@@ -12,15 +12,29 @@ then
 	curl -s -L -O $FFMPEG_TARBALL_URL
 fi
 
+# LAME configuration
+LAME_VERSION=3.100
+LAME_TARBALL=lame-$LAME_VERSION.tar.gz
+LAME_URL=https://downloads.sourceforge.net/project/lame/lame/$LAME_VERSION/$LAME_TARBALL
+
+if [ ! -e $LAME_TARBALL ]; then
+    echo "Downloading Lame $LAME_VERSION..."
+    curl -s -L -o $LAME_TARBALL $LAME_URL
+fi
+
 : ${ARCH?}
 
 OUTPUT_DIR=artifacts/ffmpeg-$FFMPEG_VERSION-audio-$ARCH-linux-gnu
+
+LAME_HOST=""
+LAME_CFLAGS=""
 
 case $ARCH in
     x86_64)
         ;;
     i686)
         FFMPEG_CONFIGURE_FLAGS+=(--cc="gcc -m32")
+        LAME_CFLAGS="-m32"
         ;;
     arm64)
         FFMPEG_CONFIGURE_FLAGS+=(
@@ -29,6 +43,7 @@ case $ARCH in
             --target-os=linux
             --arch=aarch64
         )
+        LAME_HOST="aarch64-linux-gnu"
         ;;
     arm*)
         FFMPEG_CONFIGURE_FLAGS+=(
@@ -37,6 +52,7 @@ case $ARCH in
             --target-os=linux
             --arch=arm
         )
+        LAME_HOST="arm-linux-gnueabihf"
         case $ARCH in
             armv7-a)
                 FFMPEG_CONFIGURE_FLAGS+=(
@@ -53,12 +69,14 @@ case $ARCH in
                     --cpu=cortex-a7
                     --extra-cflags='-fPIC -mcpu=cortex-a7 -mfloat-abi=hard -mfpu=neon-vfpv4 -mvectorize-with-neon-quad'
                 )
+                LAME_CFLAGS="-fPIC -mcpu=cortex-a7 -mfloat-abi=hard -mfpu=neon-vfpv4"
                 ;;
             armhf-rpi3)
                 FFMPEG_CONFIGURE_FLAGS+=(
                     --cpu=cortex-a53
                     --extra-cflags='-fPIC -mcpu=cortex-a53 -mfloat-abi=hard -mfpu=neon-fp-armv8 -mvectorize-with-neon-quad'
                 )
+                LAME_CFLAGS="-fPIC -mcpu=cortex-a53 -mfloat-abi=hard -mfpu=neon-fp-armv8"
                 ;;
         esac
         ;;
@@ -71,10 +89,55 @@ esac
 BUILD_DIR=$(mktemp -d -p $(pwd) build.XXXXXXXX)
 trap 'rm -rf $BUILD_DIR' EXIT
 
+# Build dependencies directory
+DEPS_DIR=$BASE_DIR/deps-$ARCH
+mkdir -p $DEPS_DIR
+
+# Build LAME
+echo "Building Lame..."
+cd $BUILD_DIR
+tar -xf $BASE_DIR/$LAME_TARBALL
+cd lame-$LAME_VERSION
+./configure \
+    --prefix=$DEPS_DIR \
+    ${LAME_HOST:+--host=$LAME_HOST} \
+    --disable-shared \
+    --enable-static \
+    --disable-frontend \
+    ${LAME_CFLAGS:+CFLAGS="$LAME_CFLAGS"}
+make -j$(nproc 2>/dev/null || echo 4)
+make install
+
+# Create pkg-config file for Lame
+mkdir -p $DEPS_DIR/lib/pkgconfig
+cat > $DEPS_DIR/lib/pkgconfig/libmp3lame.pc <<EOF
+prefix=$DEPS_DIR
+exec_prefix=\${prefix}
+libdir=\${exec_prefix}/lib
+includedir=\${prefix}/include
+
+Name: libmp3lame
+Description: High quality MPEG Audio Layer III (MP3) encoder
+Version: $LAME_VERSION
+Libs: -L\${libdir} -lmp3lame
+Cflags: -I\${includedir}
+EOF
+
+export PKG_CONFIG_PATH=$DEPS_DIR/lib/pkgconfig:${PKG_CONFIG_PATH:-}
+cd $BUILD_DIR
+
 cd $BUILD_DIR
 tar --strip-components=1 -xf $BASE_DIR/$FFMPEG_TARBALL
 
-FFMPEG_CONFIGURE_FLAGS+=(--prefix=$BASE_DIR/$OUTPUT_DIR)
+FFMPEG_CONFIGURE_FLAGS+=(
+    --prefix=$BASE_DIR/$OUTPUT_DIR
+    --extra-cflags="-I$DEPS_DIR/include"
+    --extra-ldflags="-L$DEPS_DIR/lib"
+    --enable-gpl
+    --enable-libmp3lame
+    --enable-encoder=libmp3lame
+    --enable-filter=aresample
+)
 
 ./configure "${FFMPEG_CONFIGURE_FLAGS[@]}" || (cat ffbuild/config.log && exit 1)
 
--- build-windows.sh
+++ build-windows.sh
@@ -12,6 +12,16 @@ then
 	curl -s -L -O $FFMPEG_TARBALL_URL
 fi
 
+# LAME configuration
+LAME_VERSION=3.100
+LAME_TARBALL=lame-$LAME_VERSION.tar.gz
+LAME_URL=https://downloads.sourceforge.net/project/lame/lame/$LAME_VERSION/$LAME_TARBALL
+
+if [ ! -e $LAME_TARBALL ]; then
+    echo "Downloading Lame $LAME_VERSION..."
+    curl -s -L -o $LAME_TARBALL $LAME_URL
+fi
+
 : ${ARCH?}
 
 OUTPUT_DIR=artifacts/ffmpeg-$FFMPEG_VERSION-audio-$ARCH-w64-mingw32
@@ -19,19 +29,59 @@ OUTPUT_DIR=artifacts/ffmpeg-$FFMPEG_VERSION-audio-$ARCH-w64-mingw32
 BUILD_DIR=$(mktemp -d -p $(pwd) build.XXXXXXXX)
 trap 'rm -rf $BUILD_DIR' EXIT
 
+# Build dependencies directory
+DEPS_DIR=$BASE_DIR/deps-$ARCH
+mkdir -p $DEPS_DIR
+
+# Build LAME
+echo "Building Lame..."
+cd $BUILD_DIR
+tar -xf $BASE_DIR/$LAME_TARBALL
+cd lame-$LAME_VERSION
+./configure \
+    --prefix=$DEPS_DIR \
+    --host=$ARCH-w64-mingw32 \
+    --disable-shared \
+    --enable-static \
+    --disable-frontend
+make -j$(nproc 2>/dev/null || echo 4)
+make install
+
+# Create pkg-config file for Lame (since 3.100 doesn't install one)
+mkdir -p $DEPS_DIR/lib/pkgconfig
+cat > $DEPS_DIR/lib/pkgconfig/libmp3lame.pc <<EOF
+prefix=$DEPS_DIR
+exec_prefix=\${prefix}
+libdir=\${exec_prefix}/lib
+includedir=\${prefix}/include
+
+Name: libmp3lame
+Description: High quality MPEG Audio Layer III (MP3) encoder
+Version: $LAME_VERSION
+Libs: -L\${libdir} -lmp3lame
+Cflags: -I\${includedir}
+EOF
+
+export PKG_CONFIG_PATH=$DEPS_DIR/lib/pkgconfig:${PKG_CONFIG_PATH:-}
+
 cd $BUILD_DIR
 tar --strip-components=1 -xf $BASE_DIR/$FFMPEG_TARBALL
 
 FFMPEG_CONFIGURE_FLAGS+=(
     --prefix=$BASE_DIR/$OUTPUT_DIR
-    --extra-cflags='-static -static-libgcc -static-libstdc++'
+    --extra-cflags="-static -static-libgcc -static-libstdc++ -I$DEPS_DIR/include"
+    --extra-ldflags="-L$DEPS_DIR/lib"
     --target-os=mingw32
     --arch=$ARCH
     --cross-prefix=$ARCH-w64-mingw32-
+    --enable-gpl
+    --enable-libmp3lame
+    --enable-encoder=libmp3lame
+    --enable-filter=aresample
 )
 
 ./configure "${FFMPEG_CONFIGURE_FLAGS[@]}"
-make
+make -j$(nproc 2>/dev/null || echo 4)
 make install
 
 chown $(stat -c '%u:%g' $BASE_DIR) -R $BASE_DIR/$OUTPUT_DIR
