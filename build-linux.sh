d9a1df6e26f964634b6285a229d1836033e24743
dqsq2e2 <sjc2523796176@qq.com>
2026-04-18 23:05:12 +0800

    Add files via upload

--- build-linux.sh
+++ build-linux.sh
@@ -79,76 +79,6 @@ fi
 
 OUTPUT_DIR=artifacts/ffmpeg-$FFMPEG_VERSION-audio-$ARCH-linux-gnu
 
-LAME_HOST=""
-LAME_CFLAGS=""
-OPENSSL_TARGET=""
-OPENSSL_EXTRA_FLAGS=""
-OPENSSL_CROSS_COMPILE=""
-
-case $ARCH in
-    x86_64)
-        OPENSSL_TARGET="linux-x86_64"
-        ;;
-    i686)
-        FFMPEG_CONFIGURE_FLAGS+=(--cc="gcc -m32")
-        LAME_CFLAGS="-m32"
-        OPENSSL_TARGET="linux-x86"
-        OPENSSL_EXTRA_FLAGS="-m32"
-        ;;
-    arm64)
-        FFMPEG_CONFIGURE_FLAGS+=(
-            --enable-cross-compile
-            --cross-prefix=aarch64-linux-gnu-
-            --target-os=linux
-            --arch=aarch64
-        )
-        LAME_HOST="aarch64-linux-gnu"
-        OPENSSL_TARGET="linux-aarch64"
-        OPENSSL_CROSS_COMPILE="aarch64-linux-gnu-"
-        ;;
-    arm*)
-        FFMPEG_CONFIGURE_FLAGS+=(
-            --enable-cross-compile
-            --cross-prefix=arm-linux-gnueabihf-
-            --target-os=linux
-            --arch=arm
-        )
-        LAME_HOST="arm-linux-gnueabihf"
-        OPENSSL_TARGET="linux-armv4"
-        OPENSSL_CROSS_COMPILE="arm-linux-gnueabihf-"
-        case $ARCH in
-            armv7-a)
-                FFMPEG_CONFIGURE_FLAGS+=(
-                    --cpu=armv7-a
-                )
-                ;;
-            armv8-a)
-                FFMPEG_CONFIGURE_FLAGS+=(
-                    --cpu=armv8-a
-                )
-                ;;
-            armhf-rpi2)
-                FFMPEG_CONFIGURE_FLAGS+=(
-                    --cpu=cortex-a7
-                    --extra-cflags='-fPIC -mcpu=cortex-a7 -mfloat-abi=hard -mfpu=neon-vfpv4 -mvectorize-with-neon-quad'
-                )
-                LAME_CFLAGS="-fPIC -mcpu=cortex-a7 -mfloat-abi=hard -mfpu=neon-vfpv4"
-                ;;
-            armhf-rpi3)
-                FFMPEG_CONFIGURE_FLAGS+=(
-                    --cpu=cortex-a53
-                    --extra-cflags='-fPIC -mcpu=cortex-a53 -mfloat-abi=hard -mfpu=neon-fp-armv8 -mvectorize-with-neon-quad'
-                )
-                LAME_CFLAGS="-fPIC -mcpu=cortex-a53 -mfloat-abi=hard -mfpu=neon-fp-armv8"
-                ;;
-        esac
-        ;;
-    *)
-        echo "Unknown architecture: $ARCH"
-        exit 1
-        ;;
-esac
-
 BUILD_DIR=$(mktemp -d -p $(pwd) build.XXXXXXXX)
 trap 'rm -rf $BUILD_DIR' EXIT
 
@@ -162,24 +92,13 @@ cd $BUILD_DIR
 tar -xf $BASE_DIR/$OPENSSL_TARBALL
 cd openssl-$OPENSSL_VERSION
 
-# Configure OpenSSL with cross-compile support
-OPENSSL_CONFIGURE_CMD="./Configure $OPENSSL_TARGET \
+# Configure OpenSSL for native build (auto-detect platform)
+./Configure \
     --prefix=$DEPS_DIR \
     --openssldir=$DEPS_DIR/ssl \
     no-shared \
-    no-tests"
+    no-tests
 
-# Add cross-compile prefix if set
-if [ -n "$OPENSSL_CROSS_COMPILE" ]; then
-    OPENSSL_CONFIGURE_CMD="$OPENSSL_CONFIGURE_CMD --cross-compile-prefix=$OPENSSL_CROSS_COMPILE"
-fi
-
-# Add extra flags if set
-if [ -n "$OPENSSL_EXTRA_FLAGS" ]; then
-    OPENSSL_CONFIGURE_CMD="$OPENSSL_CONFIGURE_CMD $OPENSSL_EXTRA_FLAGS"
-fi
-
-eval $OPENSSL_CONFIGURE_CMD
 make -j$(nproc 2>/dev/null || echo 4)
 make install_sw install_ssldirs
 
@@ -235,11 +154,9 @@ tar -xf $BASE_DIR/$LAME_TARBALL
 cd lame-$LAME_VERSION
 ./configure \
     --prefix=$DEPS_DIR \
-    ${LAME_HOST:+--host=$LAME_HOST} \
     --disable-shared \
     --enable-static \
-    --disable-frontend \
-    ${LAME_CFLAGS:+CFLAGS="$LAME_CFLAGS"}
+    --disable-frontend
 make -j$(nproc 2>/dev/null || echo 4)
 make install
 
